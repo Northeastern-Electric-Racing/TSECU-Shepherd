57709fec9da6d25b174cb188caf7e65b04dc713e
DimitryP6 <dimitryolegpianykh@gmail.com>
2025-10-14 00:49:41 -0400

    #16: Added basic implementation for initializing and running a sanitizer of cell temperatures.

---
+++ Core/Inc/cell_temp_sanitizer.h
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include "timer.h"
+#ifndef CELL_TEMP_SANITIZER_H
+#define CELL_TEMP_SANITIZER_H
+
+/**
+ * @brief A therm_state_t is a struct of a (float, bool).
+ * - Last recorded temperature of a cell.
+ * - Whether the cell temperature can be used (i.e. whether the measurement is bad).
+ */
+typedef struct {
+	float temp;
+	bool is_valid;
+	nertimer_t debounce_timer;
+} therm_state_t;
+
+/**
+ * @brief Marks the given therm_state_t as invalid.
+ */
+void mark_invalid(void *arg);
+
+/**
+ * @brief Given a grid of temperature values for cells, creates a grid of therm_t.
+ * @param temps_in are the cell temperatures.
+ * @param num_chips is the number of chips in the cell temperature array.
+ * @param num_cells is the number of cells in the cell temperature array.
+ * @param sanitized_out is the output array of therm_t, which gets initialized by this function.
+ */
+void temp_sanitizer_init(int num_chips, int num_cells,
+			 therm_state_t sanitized_out[num_chips][num_cells]);
+
+/**
+ * @brief Given a grid of cell temperatures, updates the given grid of sanitized cell temperatures.
+ * Should be run periodically.
+ * @param temps_in are the cell temperatures.
+ * @param num_chips is the number of chips in the cell temperature array.
+ * @param num_cells is the number of cells in the cell temperature array.
+ * @param sanitized_out is the output array of therm_t, which gets initialized by this function.
+ */
+void temp_sanitizer_run(int num_chips, int num_cells,
+			float temps_in[num_chips][num_cells],
+			therm_state_t sanitized_out[num_chips][num_cells]);
+
+#endif // CEL_TEMP_SANITIZER_H
---
+++ Core/Src/cell_temp_sanitizer.c
@@ -0,0 +1,61 @@
+#include "cell_temp_sanitizer.h"
+#include "debounce.h"
+#include <float.h>
+#include <stdlib.h>
+
+#define TEMP_LOWER_BOUND   20.0f
+#define TEMP_UPPER_BOUND   80.0f
+#define DEBOUNCE_PERIOD_MS 5000
+
+typedef struct {
+	float last_good_temp;
+	bool is_valid;
+	nertimer_t debounce_timer;
+} therm_state_t;
+
+void mark_invalid(void *arg)
+{
+	if (arg != NULL) {
+		((therm_state_t *)arg)->is_valid = false;
+	}
+}
+
+void temp_sanitizer_init(int num_chips, int num_cells,
+			 therm_state_t sanitized_out[num_chips][num_cells])
+{
+	for (int chip = 0; chip < num_chips; chip++) {
+		for (int cell = 0; cell < num_cells; cell++) {
+			sanitized_out[chip][cell].last_good_temp =
+				(TEMP_LOWER_BOUND + TEMP_UPPER_BOUND) / 2.0;
+			sanitized_out[chip][cell].is_valid = true;
+			sanitized_out[chip][cell].debounce_timer.active = false;
+			sanitized_out[chip][cell].debounce_timer.completed =
+				false;
+		}
+	}
+}
+
+void temp_sanitizer_run(int num_chips, int num_cells,
+			float temps_in[num_chips][num_cells],
+			therm_state_t sanitized_out[num_chips][num_cells])
+{
+	for (int chip = 0; chip < num_chips; chip++) {
+		for (int cell = 0; cell < num_cells; cell++) {
+			therm_state_t *therm_state = &sanitized_out;
+			float cell_temp = temps_in[chip][cell];
+			bool is_cell_temp_bad =
+				isnan(cell_temp) ||
+				(cell_temp > TEMP_UPPER_BOUND) ||
+				(cell_temp < TEMP_LOWER_BOUND);
+			// Start debounce timer if bad, end debounce timer if good,
+			// mark invalid if debounce timer over and still bad.
+			debounce(is_cell_temp_bad, &therm_state->debounce_timer,
+				 DEBOUNCE_PERIOD_MS, mark_invalid, therm_state);
+			// If the cell temperature is good, update cell temp and cell validity.
+			if (!is_cell_temp_bad) {
+				therm_state->last_good_temp = cell_temp;
+				therm_state->is_valid = true;
+			}
+		}
+	}
+}
