2a817394542487d4bfb203aa03d48b47d84630b3
KSMehta11 <kaushikmehta119@gmail.com>
2025-11-02 18:42:24 -0500

    Test gpo

--- Core/Inc/adi2950_interaction.h
+++ Core/Inc/adi2950_interaction.h
@@ -13,9 +13,9 @@ void read_vbat_regsisters(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi);
 
 void read_ivbat_regsisters(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi);
 
-void set_gpo(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi, GPO_2950 gpo);
-
 void reset_gpo(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi, GPO_2950 gpo);
+
+void set_gpo(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi, GPO_2950 gpo);
 
 void read_vr_registers(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi);
 
--- Core/Src/adi2950_interaction.c
+++ Core/Src/adi2950_interaction.c
@@ -57,31 +57,31 @@ void read_vr_registers(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi)
 	adBmsReadData2950(TOTAL_IC_2950, ic, RDV2C, GPV2, E_2950);
 }
 
-void set_gpo(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi, GPO_2950 gpo)
+void reset_gpo(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi, GPO_2950 gpo)
 {
 	switch (gpo) {
 	case GPO1_2950:
-		ic->tx_cfga.gpo1od = PUSH_PULL;
+		ic->tx_cfga.gpo1od = OPEN_DRAIN;
 		ic->tx_cfga.gpo1c = PULLED_UP_TRISTATED;
 		break;
 	case GPO2_2950:
-		ic->tx_cfga.gpo2od = PUSH_PULL;
+		ic->tx_cfga.gpo2od = OPEN_DRAIN;
 		ic->tx_cfga.gpo2c = PULLED_UP_TRISTATED;
 		break;
 	case GPO3_2950:
-		ic->tx_cfga.gpo3od = PUSH_PULL;
+		ic->tx_cfga.gpo3od = OPEN_DRAIN;
 		ic->tx_cfga.gpo3c = PULLED_UP_TRISTATED;
 		break;
 	case GPO4_2950:
-		ic->tx_cfga.gpo4od = PUSH_PULL;
+		ic->tx_cfga.gpo4od = OPEN_DRAIN;
 		ic->tx_cfga.gpo4c = PULLED_UP_TRISTATED;
 		break;
 	case GPO5_2950:
-		ic->tx_cfga.gpo5od = PUSH_PULL;
+		ic->tx_cfga.gpo5od = OPEN_DRAIN;
 		ic->tx_cfga.gpo5c = PULLED_UP_TRISTATED;
 		break;
 	case GPO6_2950:
-		ic->tx_cfga.gpo6od = PUSH_PULL;
+		ic->tx_cfga.gpo6od = OPEN_DRAIN;
 		ic->tx_cfga.gpo6c = PULLED_UP_TRISTATED;
 		break;
 	default:
@@ -93,32 +93,32 @@ void set_gpo(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi, GPO_2950 gpo)
 	adBmsReadData2950(TOTAL_IC_2950, ic, RDCFGA2950, Config2950, A_2950);
 }
 
-void reset_gpo(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi, GPO_2950 gpo)
+void set_gpo(cell_asic_2950 *ic, SPI_HandleTypeDef *hspi, GPO_2950 gpo)
 {
 	switch (gpo) {
 	case GPO1_2950:
-        ic->tx_cfga.gpo1od = PUSH_PULL;
+        ic->tx_cfga.gpo1od = OPEN_DRAIN;
 
 		ic->tx_cfga.gpo1c = PULLED_DOWN;
 		break;
 	case GPO2_2950:
-        ic->tx_cfga.gpo2od = PUSH_PULL;
+        ic->tx_cfga.gpo2od = OPEN_DRAIN;
 		ic->tx_cfga.gpo2c = PULLED_DOWN;
 		break;
 	case GPO3_2950:
-        ic->tx_cfga.gpo3od = PUSH_PULL;
+        ic->tx_cfga.gpo3od = OPEN_DRAIN;
 		ic->tx_cfga.gpo3c = PULLED_DOWN;
 		break;
 	case GPO4_2950:
-        ic->tx_cfga.gpo4od = PUSH_PULL;
+        ic->tx_cfga.gpo4od = OPEN_DRAIN;
 		ic->tx_cfga.gpo4c = PULLED_DOWN;
 		break;
 	case GPO5_2950:
-        ic->tx_cfga.gpo5od = PUSH_PULL;
+        ic->tx_cfga.gpo5od = OPEN_DRAIN;
 		ic->tx_cfga.gpo5c = PULLED_DOWN;
 		break;
 	case GPO6_2950:
-        ic->tx_cfga.gpo6od = PUSH_PULL;
+        ic->tx_cfga.gpo6od = OPEN_DRAIN;
 		ic->tx_cfga.gpo6c = PULLED_DOWN;
 		break;
 	default:
