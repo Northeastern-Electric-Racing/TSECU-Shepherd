06bbb7bfc9c70b119d31e3985b561bfbd707e5e8
Caio <caiodasilva05@outlook.com>
2025-11-20 07:16:37 -0500

    passing

--- Tests/Src/test_state_machine.c
+++ Tests/Src/test_state_machine.c
@@ -34,12 +34,12 @@ void test_should_charge(void) {
 
     is_timer_active_IgnoreAndReturn(false);
     start_timer_Ignore();
-    TEST_ASSERT_EQUAL_INT(false, sm_charging_check(bmsdata));
+    TEST_ASSERT_EQUAL_INT(true, sm_charging_check(bmsdata));
 }
 
 void test_charger_not_connected(void) {
     bmsdata->is_charger_connected = false;
-    TEST_ASSERT_EQUAL_INT(true, sm_charging_check(bmsdata));
+    TEST_ASSERT_EQUAL_INT(false, sm_charging_check(bmsdata));
 }
 
 int main(void) {
