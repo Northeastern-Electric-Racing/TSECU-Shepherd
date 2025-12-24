a6e834dcda36759d8522cb215adf80a1c5d2b9e5
Caio <caiodasilva05@outlook.com>
2025-12-23 19:33:57 -0500

    format

--- Core/Src/compute.c
+++ Core/Src/compute.c
@@ -22,10 +22,11 @@ bool read_shutdown()
 {
 	// Read shutdown sense using TS_MINUS_SENSE pin
 	bool shutdown =
-		HAL_GPIO_ReadPin(TS_MINUS_SENSE_GPIO_Port, TS_MINUS_SENSE_Pin) && 
-    HAL_GPIO_ReadPin(TS_PLUS_SENSE_GPIO_Port, TS_PLUS_SENSE_Pin) &&
-    HAL_GPIO_ReadPin(ACC_SENSE_GPIO_Port, ACC_SENSE_Pin) &&
-    HAL_GPIO_ReadPin(TSIP_SENSE_GPIO_Port, TSIP_SENSE_Pin);
+		HAL_GPIO_ReadPin(TS_MINUS_SENSE_GPIO_Port,
+				 TS_MINUS_SENSE_Pin) &&
+		HAL_GPIO_ReadPin(TS_PLUS_SENSE_GPIO_Port, TS_PLUS_SENSE_Pin) &&
+		HAL_GPIO_ReadPin(ACC_SENSE_GPIO_Port, ACC_SENSE_Pin) &&
+		HAL_GPIO_ReadPin(TSIP_SENSE_GPIO_Port, TSIP_SENSE_Pin);
 
 	// If the pin is high, the shutdown circuit is closed. So, return false.
 	// If the pin is low, the shutdown circuit is open. So, return true.
