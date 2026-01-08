a58d5b3f3819cd55d0f1c0c51067b7aaf00ed64b
Caio <caiodasilva05@outlook.com>
2026-01-07 23:16:27 -0500

    5 - fmt

--- Core/Src/soc.c
+++ Core/Src/soc.c
@@ -89,7 +89,7 @@ void update_soc(analyzer_t *analyzer, hv_plate_t *hv_plate)
 	float current = hv_plate->pack_current; // in Amperes
 
 	// subtracted since discharging current is positive
-	float soc =  last_soc - (current * delta_time) / FULL_CAPACITY_AH;
+	float soc = last_soc - (current * delta_time) / FULL_CAPACITY_AH;
 	if (soc > 1.0f) {
 		soc = 1.0f;
 	} else if (soc < 0.0f) {
