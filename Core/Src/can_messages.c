e578d2e36a5ee2fe258724351a4dfe7981fda43c
Caio <caiodasilva05@outlook.com>
2026-01-10 14:09:09 -0500

    fmt

--- Core/Src/can_messages.c
+++ Core/Src/can_messages.c
@@ -295,11 +295,16 @@ void send_segment_total_volt_message(analyzer_t *analyzer)
 	uint8_t bitstream_data[8];
 	bitstream_init(&segment_total_volt_msg, bitstream_data, 8);
 
-	bitstream_add(&segment_total_volt_msg, analyzer->segment_total_volts[0] * 39, 12); // Segment 1
-	bitstream_add(&segment_total_volt_msg, analyzer->segment_total_volts[1] * 39, 12); // Segment 2
-	bitstream_add(&segment_total_volt_msg, analyzer->segment_total_volts[2] * 39, 12); // Segment 3
-	bitstream_add(&segment_total_volt_msg, analyzer->segment_total_volts[3] * 39, 12); // Segment 4
-	bitstream_add(&segment_total_volt_msg, analyzer->segment_total_volts[4] * 39, 12); // Segment 5
+	bitstream_add(&segment_total_volt_msg,
+		      analyzer->segment_total_volts[0] * 39, 12); // Segment 1
+	bitstream_add(&segment_total_volt_msg,
+		      analyzer->segment_total_volts[1] * 39, 12); // Segment 2
+	bitstream_add(&segment_total_volt_msg,
+		      analyzer->segment_total_volts[2] * 39, 12); // Segment 3
+	bitstream_add(&segment_total_volt_msg,
+		      analyzer->segment_total_volts[3] * 39, 12); // Segment 4
+	bitstream_add(&segment_total_volt_msg,
+		      analyzer->segment_total_volts[4] * 39, 12); // Segment 5
 	bitstream_add(&segment_total_volt_msg, 0, 4); // Extra (4 bits)
 
 	can_msg_t msg;
