83ba811d255957b9a6f6cc50d048eb9b3d7ad242
Surya Thoppae <63197592+suryatho@users.noreply.github.com>
2026-01-14 16:46:37 -0500

    Move initialization of pwm handle into control.c #48

--- Core/Inc/control.h
+++ Core/Inc/control.h
@@ -1,46 +1,34 @@
 #ifndef _CONTROL_H
 #define _CONTROL_H
 
+#include "fdcan.h"
+#include "stm32h5xx_hal.h"
 #include <stdint.h>
 
-#define _PERCENT_16(x) ((uint16_t)(x * ((1 << 16) - 1) / 100.0f))
-typedef enum {
-	PWM_DUTY_0,
-	PWM_DUTY_10 = _PERCENT_16(10),
-	PWM_DUTY_20 = _PERCENT_16(20),
-	PWM_DUTY_30 = _PERCENT_16(30),
-	PWM_DUTY_40 = _PERCENT_16(40),
-	PWM_DUTY_50 = _PERCENT_16(50),
-	PWM_DUTY_60 = _PERCENT_16(60),
-	PWM_DUTY_70 = _PERCENT_16(70),
-	PWM_DUTY_80 = _PERCENT_16(80),
-	PWM_DUTY_90 = _PERCENT_16(90),
-	PWM_DUTY_100 = _PERCENT_16(100),
-} pwm_duty_t;
-#undef _PERCENT_16
+#define CONTROL_MIN_DUTY_FAN 20
 
-#define CONTROL_MAX_TEMP_NO_FAN 20.0f
-#define CONTROL_MAX_TEMP_20_FAN 30.0f
-#define CONTROL_MAX_TEMP_40_FAN 40.0f
-#define CONTROL_MAX_TEMP_60_FAN 50.0f
-#define CONTROL_MAX_TEMP_80_FAN 60.0f
+typedef struct {
+  TIM_HandleTypeDef *tim_handle;
+  int channel_identifier;
+} pwm_device_t;
+
+typedef enum { DEVICE_FAN0, NUM_DEVICES } control_devices;
 
 /**
  * @brief Initialize i/o for control
  *
+ * @return Returns false if failed to init one or more peripherals
  */
-void control_init(void);
+bool control_init_peripherals(void);
 
 /**
- * @brief Write pwm signal to fan
- *
+ * @brief Do control based on max temp from analyzer
  */
-void write_fan_duty_cycle(pwm_duty_t pwm_duty);
+void handle_max_temp(float max_temp);
 
 /**
- * @brief Control fan based on segement average temps
- *
+ * @brief Send calypso message for controlling fans
  */
-void handle_segement_average_temps(float *segment_average_temps);
+void control_message_fans(can_msg_t msg);
 
 #endif
