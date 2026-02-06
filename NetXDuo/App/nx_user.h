1b4c32723f517feb4644b8b46189d2fe0597ac7f
Surya Thoppae <63197592+suryatho@users.noreply.github.com>
2026-02-05 20:19:51 -0500

    Fix merge

--- NetXDuo/App/nx_user.h
+++ NetXDuo/App/nx_user.h
@@ -818,7 +818,9 @@
 /* Specifies the maximum number of multicast groups that can be joined.
    The default value is 7 and is defined in nx_api.h. The application can
    override the default by defining the value before nx_api.h is included. */
-#define NX_MAX_MULTICAST_GROUPS		 			256
+/*
+#define NX_MAX_MULTICAST_GROUPS     			7
+*/
 
 /*****************************************************************************/
 /******************* Configuration options for ARP/RARP **********************/
