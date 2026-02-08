df4f46e02709d8e9f7f54f5f4769d76b2ff58e5e
lucy-shah <lucyshah008@gmail.com>
2026-02-08 16:28:43 -0500

    Add compute_t struct and init_compute function (#57)
    Co-authored-by: Caio <caiodasilva05@outlook.com>
    Co-authored-by: Caio DaSilva <145620095+caiodasilva2005@users.noreply.github.com>

--- Core/Src/compute.c
+++ Core/Src/compute.c
@@ -1,8 +1,6 @@
-
 #include "compute.h"
-
 #include <assert.h>
-
+#include <stdlib.h>
 #include "datastructs.h"
 #include "main.h"
 #include <sht30.h>
@@ -228,6 +226,48 @@ int imu_getAngularRate(vector3_t *data)
 	data->z = lsm6dsv_from_fs2000_to_mdps(raw_data[2]);
 
 	return U_SUCCESS;
+}
+
+typedef struct {
+	sht30_t sht30;
+} compute_t;
+
+compute_t *compute;
+extern I2C_HandleTypeDef hi2c1;
+
+// moving the bulk of the pointers over
+static inline uint8_t sht30_i2c_write(uint8_t *data, uint8_t dev_address,
+
+				      uint8_t length)
+{
+	return HAL_I2C_Master_Transmit(&hi2c1, dev_address, data, length,
+				       HAL_MAX_DELAY);
+}
+static inline uint8_t sht30_i2c_read(uint8_t *data, uint16_t command,
+				     uint8_t dev_address, uint8_t length)
+{
+	return HAL_I2C_Mem_Read(&hi2c1, dev_address, command, sizeof(command),
+				data, length, HAL_MAX_DELAY);
+}
+static inline uint8_t sht30_i2c_blocking_read(uint8_t *data, uint16_t command,
+					      uint8_t dev_address,
+					      uint8_t length)
+{
+	uint8_t command_buffer[2] = { (command & 0xff00u) >> 8u,
+				      command & 0xffu };
+	sht30_i2c_write(command_buffer, dev_address, sizeof(command_buffer));
+	HAL_Delay(1);
+	return HAL_I2C_Master_Receive(&hi2c1, dev_address, data, length,
+				      HAL_MAX_DELAY);
+}
+
+void init_compute(compute_t *compute_ptr)
+{
+	assert(compute_ptr);
+	assert(!sht30_init(&compute_ptr->sht30, (Write_ptr)sht30_i2c_write,
+			   (Read_ptr)sht30_i2c_read,
+			   (Read_ptr)sht30_i2c_blocking_read, SHT30_I2C_ADDR));
+	compute = compute_ptr;
 }
 
 void compute_set_fault(bool fault_state)
