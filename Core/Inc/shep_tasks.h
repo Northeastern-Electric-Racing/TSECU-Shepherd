452d8e7a8fabfc8dff62806dee8b3bb6ade10138
KSMehta11 <kaushikmehta119@gmail.com>
2026-02-08 11:46:03 -0800

    Fixed formatting

--- Core/Inc/shep_tasks.h
+++ Core/Inc/shep_tasks.h
@@ -5,11 +5,13 @@
 #include "tx_api.h"
 #include "datastructs.h"
 
-#define ANALYZER_FLAG  (1U << 0)
-#define SANITIZER_FLAG (1U << 1)
-#define DEBUG_FLAG     (1U << 2)
-#define SPI_LINE_A_DMA_RX_CPLT_FLAG (1U << 3)
-#define SPI_LINE_B_DMA_RX_CPLT_FLAG (1U << 4)
+// clang-format off
+#define ANALYZER_FLAG		         (1U << 0)
+#define SANITIZER_FLAG		         (1U << 1)
+#define DEBUG_FLAG		             (1U << 2)
+#define SPI_LINE_A_DMA_RX_CPLT_FLAG  (1U << 3)
+#define SPI_LINE_B_DMA_RX_CPLT_FLAG  (1U << 4)
+// clang-format on
 
 // #define DEBUG_HV_PLATE
 // #define DEBUG_VOLTAGES
