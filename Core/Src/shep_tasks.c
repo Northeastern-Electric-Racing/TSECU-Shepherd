b1142a7232126e49619c538332f1f2c358734b57
ninjadrknss <116192448+ninjadrknss@users.noreply.github.com>
2026-02-08 22:32:29 -0500

    undoing the debugging in shep_tasks

--- Core/Src/shep_tasks.c
+++ Core/Src/shep_tasks.c
@@ -121,8 +121,7 @@ void vDefaultTask(ULONG thread_input)
 		alt = !alt;
 
 		//HAL_IWDG_Refresh(&hiwdg);
-		// tx_thread_sleep(MS_TO_TICKS(2000));
-		Delay_ms(2000);
+		tx_thread_sleep(MS_TO_TICKS(200));
 	}
 }
 
@@ -209,7 +208,6 @@ void vCanDispatch(ULONG thread_input)
 
 void vAnalyzer(ULONG thread_input)
 {
-
 	PRINTLN_INFO("Starting Analyzer Thread...");
 
 	analyzer_args_t *analyzer_args = (analyzer_args_t *)thread_input;
@@ -253,7 +251,7 @@ void vAnalyzer(ULONG thread_input)
 }
 
 void vGetSegmentData(ULONG thread_input)
-{	
+{
 	PRINTLN_INFO("Starting GetSegmentData thread...");
 
 	const uint16_t balancing_delay = 75;
@@ -366,7 +364,7 @@ void vHvPlateData(ULONG thread_input)
 			dcl_calc_cont_limit(hv_plate->pack_current, bms_algos);
 			ccl_calc_cont_limit(hv_plate->pack_current, bms_algos);
 		}
-		*/	
+		*/
 
 		// read ts voltage
 		get_ts_voltage(hv_plate);
@@ -499,7 +497,6 @@ void vPeripherals(ULONG thread_input)
 
 void vDebug(ULONG thread_input)
 {
-
 	PRINTLN_INFO("Starting Debug thread...");
 
 	analyzer_t *analyzer = (analyzer_t *)thread_input;
@@ -758,14 +755,14 @@ uint8_t shep_threads_init(TX_BYTE_POOL *byte_pool)
 
 	PRINTLN_INFO("RUNNING THREADS");
 
-	/* Task Definitions End */	
+	/* Task Definitions End */
 	CATCH_ERROR(create_thread(byte_pool, &_default_thread), U_SUCCESS);
 	//CATCH_ERROR(create_thread(byte_pool, &_state_machine_thread),
 	//	    U_SUCCESS);
 	//CATCH_ERROR(create_thread(byte_pool, &_analyzer_thread), U_SUCCESS);
-	// CATCH_ERROR(create_thread(byte_pool, &_can_dispatch_thread), U_SUCCESS);
+	CATCH_ERROR(create_thread(byte_pool, &_can_dispatch_thread), U_SUCCESS);
 	//CATCH_ERROR(create_thread(byte_pool, &_can_receive_thread), U_SUCCESS);
-	// CATCH_ERROR(create_thread(byte_pool, &_segment_data_thread), U_SUCCESS);
+	CATCH_ERROR(create_thread(byte_pool, &_segment_data_thread), U_SUCCESS);
 	//CATCH_ERROR(create_thread(byte_pool, &_hv_plate_data_thread),
 	//	    U_SUCCESS);
 	//CATCH_ERROR(create_thread(byte_pool, &_sanitizer_thread), U_SUCCESS);
