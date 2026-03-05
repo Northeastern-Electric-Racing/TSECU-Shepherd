0b3de828722cba323c2495f3ce884cc434f5201a
Aolgeirson <asher@olgeirson.com>
2026-03-05 11:50:31 -0700

    draft of p3t driver inclusion

--- Core/Inc/compute.h
+++ Core/Inc/compute.h
@@ -1,22 +1,28 @@
 #ifndef _COMPUTE_H
 #define _COMPUTE_H
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
+#include "datastructs.h"
 #include "stm32h5xx.h"
 #include "u_tx_threads.h"
-#include "datastructs.h"
 
 #define CURRENT_SENSOR_PIN_L A1
 #define CURRENT_SENSOR_PIN_H A0
-#define MEAS_5VREF_PIN	     A7
-#define FAULT_PIN	     2
-#define CHARGE_SAFETY_RELAY  4
-#define CHARGE_DETECT	     5
-#define CHARGER_BAUD	     250000U
-#define MC_BAUD		     1000000U
-#define MAX_ADC_RESOLUTION   4095 // 12 bit ADC
+#define MEAS_5VREF_PIN A7
+#define FAULT_PIN 2
+#define CHARGE_SAFETY_RELAY 4
+#define CHARGE_DETECT 5
+#define CHARGER_BAUD 250000U
+#define MC_BAUD 1000000U
+#define MAX_ADC_RESOLUTION 4095 // 12 bit ADC
+#define P3T1755_DEV_ADDR                                                       \
+  0x7 // FIXME: I don't know what a sane value for this is
+
+int p3t_init(void);
+
+int p3t1755_getBoardTemp(float *temp_c);
 
 int imu_init(void);
 
@@ -47,9 +53,9 @@ void compute_set_fault(bool fault_state);
 bool read_shutdown();
 
 /**
-* @brief Initializes peripherals for compute thread.
-* @param peripherals Pointer to peripherals struct
-*/
+ * @brief Initializes peripherals for compute thread.
+ * @param peripherals Pointer to peripherals struct
+ */
 void init_compute(peripherals_t *peripherals);
 
 /**
@@ -60,7 +66,7 @@ void init_compute(peripherals_t *peripherals);
  * @return Status
  */
 int tempsensor_getTemperatureAndHumdidty(peripherals_t *peripherals,
-					 float *temperature, float *humidity);
+                                         float *temperature, float *humidity);
 
 void vPeripherals(ULONG thread_input);
 
--- Core/Inc/datastructs.h
+++ Core/Inc/datastructs.h
@@ -2,25 +2,34 @@
 #ifndef _DATA_STRUCTS_H
 #define _DATA_STRUCTS_H
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "bms_config.h"
-#include "u_tx_mutex.h"
 #include "adBms6830Data.h"
 #include "adi_bms_2950data.h"
-#include "timer.h"
+#include "bms_config.h"
 #include "sht30.h"
+#include "timer.h"
+#include "u_tx_mutex.h"
+#include <stdbool.h>
+#include <stdint.h>
 
-#define ANALYZER_FLAG  0x1
+#define ANALYZER_FLAG 0x1
 #define SANITIZER_FLAG 0x2
-#define DEBUG_FLAG     0x4
+#define DEBUG_FLAG 0x4
 
 /**
- * @brief Stores critical values for the pack (across all chips), and where that critical value can be found
+ * @brief Compute Board Temperature
+ * @note Stores the output of p3t1755 temperature sensor
  */
 typedef struct {
-	float val;
-	uint8_t chipNum;
+  float temp_c;
+} board_temp_t;
+
+/**
+ * @brief Stores critical values for the pack (across all chips), and where that
+ * critical value can be found
+ */
+typedef struct {
+  float val;
+  uint8_t chipNum;
 } crit_chipval_t;
 
 /**
@@ -28,224 +37,228 @@ typedef struct {
  * @note stores thermistor values, voltage readings, and the discharge status
  */
 typedef struct {
-	int error_reading;
+  int error_reading;
 
-	bool alpha;
-	/* whether the chip is an alpha or beta */ // TODO initialize properly
+  bool alpha;
+  /* whether the chip is an alpha or beta */ // TODO initialize properly
 
-	/* These are calculated during the analysis of data */
+  /* These are calculated during the analysis of data */
 
-	/* Cell temperature in celsius */
-	float cell_temp[NUM_CELLS_PER_CHIP];
-	float cell_resistance[NUM_CELLS_PER_CHIP];
-	float open_cell_voltage[NUM_CELLS_PER_CHIP];
-	float cell_voltages[NUM_CELLS_PER_CHIP];
+  /* Cell temperature in celsius */
+  float cell_temp[NUM_CELLS_PER_CHIP];
+  float cell_resistance[NUM_CELLS_PER_CHIP];
+  float open_cell_voltage[NUM_CELLS_PER_CHIP];
+  float cell_voltages[NUM_CELLS_PER_CHIP];
 
-	/* Maximum temperature of on-board therms.*/
-	float on_board_temp[NUM_ONBOARD_THERMS_PER_CHIP];
+  /* Maximum temperature of on-board therms.*/
+  float on_board_temp[NUM_ONBOARD_THERMS_PER_CHIP];
 
-	/// temperature of the die
-	float die_temp;
+  /// temperature of the die
+  float die_temp;
 
-	/* Chip and Cell Diagnostic Data */
-	bool is_balancing[NUM_CELLS_PER_CHIP];
-	bool cs_fault[NUM_CELLS_PER_CHIP];
+  /* Chip and Cell Diagnostic Data */
+  bool is_balancing[NUM_CELLS_PER_CHIP];
+  bool cs_fault[NUM_CELLS_PER_CHIP];
 
-	float vpv;
-	float vmv;
-	float v_res;
-	float vref2;
-	float v_analog;
-	float v_digital;
+  float vpv;
+  float vmv;
+  float v_res;
+  float vref2;
+  float v_analog;
+  float v_digital;
 
-	stc_ flt_reg;
+  stc_ flt_reg;
 
 } chipdata_t;
 
 /**
- * @brief Stores critical values for the pack, and where that critical value can be found
+ * @brief Stores critical values for the pack, and where that critical value can
+ * be found
  */
 typedef struct {
-	float val;
-	uint8_t chipIndex;
-	uint8_t cellNum;
+  float val;
+  uint8_t chipIndex;
+  uint8_t cellNum;
 } crit_cellval_t;
 
 /**
  * @brief A therm_state_t is a struct of a (float, bool).
  * - Last recorded temperature of a cell.
- * - Whether the cell temperature can be used (i.e. whether the measurement is bad).
+ * - Whether the cell temperature can be used (i.e. whether the measurement is
+ * bad).
  */
 typedef struct {
-	float last_temp;
-	bool valid;
+  float last_temp;
+  bool valid;
 } therm_state_t;
 
 /**
  * @brief States the BMS can be in
  */
 typedef enum {
-	BOOT,
-	READY,
-	CHARGING,
-	BALANCING,
-	FAULTED,
-	NUM_STATES,
+  BOOT,
+  READY,
+  CHARGING,
+  BALANCING,
+  FAULTED,
+  NUM_STATES,
 } state_t;
 
 typedef enum {
-	FAULT_TIMER_STOPPED,
-	FAULT_TIMER_STARTED,
-	FAULT_TIMER_EXPIRED,
+  FAULT_TIMER_STOPPED,
+  FAULT_TIMER_STARTED,
+  FAULT_TIMER_EXPIRED,
 } fault_timer_status_t;
 
 /**
  * @brief Data needed for the therm temp sanitizer
  */
 typedef struct {
-	therm_state_t sanitized_therms[NUM_CHIPS][NUM_CELLS_PER_CHIP];
-	crit_cellval_t max_sanitized_temp;
-	crit_cellval_t min_sanitized_temp;
+  therm_state_t sanitized_therms[NUM_CHIPS][NUM_CELLS_PER_CHIP];
+  crit_cellval_t max_sanitized_temp;
+  crit_cellval_t min_sanitized_temp;
 } sanitizer_t;
 
 /**
  * @brief Flags from adbms_2950 chip
  */
 typedef union {
-	struct {
-		uint8_t vreguv : 1;
-		uint8_t vregov : 1;
-		uint8_t vdduv : 1;
-		uint8_t vdiguv : 1;
-		uint8_t vdigov : 1;
-		uint8_t vde : 1;
-		uint8_t vdel : 1;
-		uint8_t oscflt : 1;
-		uint8_t noclk : 1;
-		uint8_t spiflt : 1;
-		uint8_t thsd : 1;
-		uint8_t reset : 1;
-	} flags;
-	uint16_t raw;
+  struct {
+    uint8_t vreguv : 1;
+    uint8_t vregov : 1;
+    uint8_t vdduv : 1;
+    uint8_t vdiguv : 1;
+    uint8_t vdigov : 1;
+    uint8_t vde : 1;
+    uint8_t vdel : 1;
+    uint8_t oscflt : 1;
+    uint8_t noclk : 1;
+    uint8_t spiflt : 1;
+    uint8_t thsd : 1;
+    uint8_t reset : 1;
+  } flags;
+  uint16_t raw;
 } adbms_2950_flags_t;
 
 /**
  * @brief data read from the ADBMS2950 on our HV Plate
  */
 typedef struct {
-	cell_asic_2950 *ic; // ADBMS2950 struct
-	float ts_volts; // TS Voltage (V)
-	float batt_volts; // BATT Voltage (V)
-	float shunt_temp; // Temperature of shunt resistor (C)
-	float pack_current; // Current read through the shunt (A)
-	uint16_t conversion_count; // Number of conversions taken for each voltage and current measurement
-	uint16_t last_total_converion_count; // previously read total conversion count
-	adbms_2950_flags_t adbms_flags; // Relevant flags from flag register
-	// AUX ADC values
-	float vreg; // VREG power supply pin (V)
-	float tmp1; // Primary internal temperature sensor (C)
-	float vref1p25; // VREF1P25 reference pin (V)
-	float epad; // Exposed pad (V)
-	float vdig; // Internal digital 3V supply (V)
-	float vdd; // VDD power supply pin (V)
-	float tmp2; // Secondary internal temperature sensor (C)
-	float vdiv; // Divided VREF1 Voltage (V)
-	uint16_t osccnt; // Oscillator count
+  cell_asic_2950 *ic;        // ADBMS2950 struct
+  float ts_volts;            // TS Voltage (V)
+  float batt_volts;          // BATT Voltage (V)
+  float shunt_temp;          // Temperature of shunt resistor (C)
+  float pack_current;        // Current read through the shunt (A)
+  uint16_t conversion_count; // Number of conversions taken for each voltage and
+                             // current measurement
+  uint16_t last_total_converion_count; // previously read total conversion count
+  adbms_2950_flags_t adbms_flags;      // Relevant flags from flag register
+  // AUX ADC values
+  float vreg;      // VREG power supply pin (V)
+  float tmp1;      // Primary internal temperature sensor (C)
+  float vref1p25;  // VREF1P25 reference pin (V)
+  float epad;      // Exposed pad (V)
+  float vdig;      // Internal digital 3V supply (V)
+  float vdd;       // VDD power supply pin (V)
+  float tmp2;      // Secondary internal temperature sensor (C)
+  float vdiv;      // Divided VREF1 Voltage (V)
+  uint16_t osccnt; // Oscillator count
 } hv_plate_t;
 
 /**
  * @brief data read from the ADBMS6830 chips on our segments
  */
 typedef struct {
-	/* Array of structs containing raw data from and configurations for the ADBMS6830 chips */
-	cell_asic chips[NUM_CHIPS];
+  /* Array of structs containing raw data from and configurations for the
+   * ADBMS6830 chips */
+  cell_asic chips[NUM_CHIPS];
 
-	// the current discharge configuration the state machine wants
-	bool discharge_config[NUM_CHIPS][NUM_CELLS_PER_CHIP];
+  // the current discharge configuration the state machine wants
+  bool discharge_config[NUM_CHIPS][NUM_CELLS_PER_CHIP];
 } acc_data_t;
 
 /**
  * @brief ISO SPI communication state machine states.
  */
 typedef enum {
-	ISOSPI_STATE_NORMAL = 0x01U,
-	ISOSPI_BREAK_DETECTED = 0x02U,
-	ISOSPI_STATE_VERIFYING = 0x03U,
-	ISOSPI_RECOVERY_SUCCESS = 0x04U,
-	ISOSPI_RECOVERY_FAILED = 0x05U
+  ISOSPI_STATE_NORMAL = 0x01U,
+  ISOSPI_BREAK_DETECTED = 0x02U,
+  ISOSPI_STATE_VERIFYING = 0x03U,
+  ISOSPI_RECOVERY_SUCCESS = 0x04U,
+  ISOSPI_RECOVERY_FAILED = 0x05U
 } isospi_comm_state_t;
 
 /**
  * @brief ISO SPI break detection and recovery status structure.
  */
 typedef struct {
-	isospi_comm_state_t state;
-	uint8_t break_chip;
-	uint8_t verification_attempts;
-	uint8_t recovery_successful;
-	uint8_t fault_latched;
+  isospi_comm_state_t state;
+  uint8_t break_chip;
+  uint8_t verification_attempts;
+  uint8_t recovery_successful;
+  uint8_t fault_latched;
 } isospi_status_t;
 
 /**
  * @brief data needed for processing raw data
  */
 typedef struct {
-	/* Array of data from all chips in the system */
-	chipdata_t chip_data[NUM_CHIPS];
+  /* Array of data from all chips in the system */
+  chipdata_t chip_data[NUM_CHIPS];
 
-	/* Max, min, and avg thermistor readings */
-	crit_cellval_t max_temp;
-	crit_cellval_t min_temp;
-	float avg_temp;
+  /* Max, min, and avg thermistor readings */
+  crit_cellval_t max_temp;
+  crit_cellval_t min_temp;
+  float avg_temp;
 
-	// the board temperature
-	float internal_temp;
+  // the board temperature
+  float internal_temp;
 
-	/* Max, min, and avg voltage of the cells */
-	crit_cellval_t max_voltage;
-	crit_cellval_t min_voltage;
-	float avg_voltage;
-	float delt_voltage;
+  /* Max, min, and avg voltage of the cells */
+  crit_cellval_t max_voltage;
+  crit_cellval_t min_voltage;
+  float avg_voltage;
+  float delt_voltage;
 
-	// OCV timer
-	nertimer_t ocvTimer;
+  // OCV timer
+  nertimer_t ocvTimer;
 
-	/* Max, min, average OCV readings */
-	crit_cellval_t max_ocv;
-	crit_cellval_t min_ocv;
-	float avg_ocv;
-	float delt_ocv;
-	float pack_ocv;
-	float pack_res;
+  /* Max, min, average OCV readings */
+  crit_cellval_t max_ocv;
+  crit_cellval_t min_ocv;
+  float avg_ocv;
+  float delt_ocv;
+  float pack_ocv;
+  float pack_res;
 
-	// the highest current chip temperature, for faulting
-	crit_chipval_t max_chiptemp;
+  // the highest current chip temperature, for faulting
+  crit_chipval_t max_chiptemp;
 
-	/* semgent temperature averages */
-	float segment_average_temps[NUM_SEGMENTS];
-	/* OCV average voltages */
-	float segment_average_volts[NUM_SEGMENTS];
-	/* Total voltages for each segment */
-	float segment_total_volts[NUM_SEGMENTS];
-	/* Delta voltages for each segment */
-	float segment_delt_volts[NUM_SEGMENTS];
+  /* semgent temperature averages */
+  float segment_average_temps[NUM_SEGMENTS];
+  /* OCV average voltages */
+  float segment_average_volts[NUM_SEGMENTS];
+  /* Total voltages for each segment */
+  float segment_total_volts[NUM_SEGMENTS];
+  /* Delta voltages for each segment */
+  float segment_delt_volts[NUM_SEGMENTS];
 
-	/* Voltage of pack */
-	float pack_voltage;
+  /* Voltage of pack */
+  float pack_voltage;
 
-	/* SoC of the Pack*/
-	float soc;
+  /* SoC of the Pack*/
+  float soc;
 } analyzer_t;
 
 /**
  * @brief data retrieved from BMS algorithms
  */
 typedef struct {
-	// All current limit values are positive
-	float cont_DCL;
-	float cont_CCL;
-	float inst_DCL;
-	float inst_CCL;
+  // All current limit values are positive
+  float cont_DCL;
+  float cont_CCL;
+  float inst_DCL;
+  float inst_CCL;
 } bms_algos_t;
 
 /**
@@ -254,8 +267,8 @@ typedef struct {
  * Defines when a cooldown period is enforced after a pulse.
  */
 typedef enum {
-	COOLDOWN_ON_FULL_PULSE = 0, // Cooldown only after full pulse
-	COOLDOWN_ALWAYS // Cooldown after any pulse exit
+  COOLDOWN_ON_FULL_PULSE = 0, // Cooldown only after full pulse
+  COOLDOWN_ALWAYS             // Cooldown after any pulse exit
 } pulse_cooldown_mode_t;
 
 /**
@@ -264,9 +277,9 @@ typedef enum {
  * Represents the high-level operating phase of the limiter.
  */
 typedef enum {
-	CURRENT_LIMIT_STATE_REST = 0, // Limiter idle
-	CURRENT_LIMIT_STATE_PULSE, // Pulse active
-	CURRENT_LIMIT_STATE_COOLDOWN // Cooldown active
+  CURRENT_LIMIT_STATE_REST = 0, // Limiter idle
+  CURRENT_LIMIT_STATE_PULSE,    // Pulse active
+  CURRENT_LIMIT_STATE_COOLDOWN  // Cooldown active
 } current_limit_algo_state_t;
 
 /**
@@ -276,10 +289,10 @@ typedef enum {
  * algorithm. It is algorithm-owned and does not represent system state.
  */
 typedef struct {
-	float min_temp;
-	float max_temp;
-	float min_ocv;
-	float max_ocv;
+  float min_temp;
+  float max_temp;
+  float min_ocv;
+  float max_ocv;
 } current_limit_algo_inputs_t;
 
 /**
@@ -290,146 +303,148 @@ typedef struct {
  * by the current limit algorithm.
  */
 typedef struct {
-	// Current limiter state
-	current_limit_algo_state_t state;
+  // Current limiter state
+  current_limit_algo_state_t state;
 
-	// Cooldown behavior mode
-	pulse_cooldown_mode_t cooldown_mode;
+  // Cooldown behavior mode
+  pulse_cooldown_mode_t cooldown_mode;
 
-	// Pulse duration timer
-	nertimer_t pulse_timer;
+  // Pulse duration timer
+  nertimer_t pulse_timer;
 
-	// Above and below threshold debounce timer
-	nertimer_t t_above;
-	nertimer_t t_below;
+  // Above and below threshold debounce timer
+  nertimer_t t_above;
+  nertimer_t t_below;
 
-	// Cooldown duration timer
-	nertimer_t cooldown_timer;
+  // Cooldown duration timer
+  nertimer_t cooldown_timer;
 
-	// Pulse allowed flag
-	bool pulse_allowed;
+  // Pulse allowed flag
+  bool pulse_allowed;
 } current_limit_pulse_ctrl_t;
 
 typedef enum {
-	LONG_CHARGE_UP,
-	LONG_SETTLE,
-	SHORT_CHARGE_UP,
-	SHORT_SETTLE,
-	DONE,
-	FAULT
+  LONG_CHARGE_UP,
+  LONG_SETTLE,
+  SHORT_CHARGE_UP,
+  SHORT_SETTLE,
+  DONE,
+  FAULT
 } charge_stage_t;
 
 /**
  * @brief data for determine the current BMS State
  */
 typedef struct {
-	state_t bms_state;
+  state_t bms_state;
 
-	/**
-	 * @brief Note that this is a 32 bit integer, so there are 32 max possible fault codes
-	 */
-	// uint32_t fault_code;
-	uint32_t fault_code_crit;
-	uint32_t fault_code_noncrit;
+  /**
+   * @brief Note that this is a 32 bit integer, so there are 32 max possible
+   * fault codes
+   */
+  // uint32_t fault_code;
+  uint32_t fault_code_crit;
+  uint32_t fault_code_noncrit;
 
-	// charge settling timers
-	nertimer_t charging_stage_timer;
-	charge_stage_t charging_stage;
+  // charge settling timers
+  nertimer_t charging_stage_timer;
+  charge_stage_t charging_stage;
 
-	// charging message timer for telemetry
-	nertimer_t charger_message_timer;
+  // charging message timer for telemetry
+  nertimer_t charger_message_timer;
 } state_machine_t;
 
 /**
  * Represents a 3D vector for IMU data
  */
 typedef struct {
-	float x;
-	float y;
-	float z;
+  float x;
+  float y;
+  float z;
 } vector3_t;
 
 typedef struct {
-	vector3_t accel_data;
-	vector3_t ang_rate_data;
+  vector3_t accel_data;
+  vector3_t ang_rate_data;
 } imu_data_t;
 
 typedef struct {
-	imu_data_t imu_data;
+  imu_data_t imu_data;
+  board_temp_t board_temp;
 } peripherals_t;
 
 /* Task Args */
 
 typedef struct {
-	hv_plate_t *hv_plate;
-	analyzer_t *analyzer;
-	bms_algos_t *bms_algos;
-	acc_data_t *acc_data;
+  hv_plate_t *hv_plate;
+  analyzer_t *analyzer;
+  bms_algos_t *bms_algos;
+  acc_data_t *acc_data;
 } default_task_args_t;
 
 /**
  * @brief args for vStateMachine
  */
 typedef struct {
-	state_machine_t *state_machine;
-	analyzer_t *analyzer;
-	hv_plate_t *
-		hv_plate; // TODO add hv plate interal data to analyzer to remove hv_plate
-	acc_data_t *acc_data;
-	bms_algos_t *bms_algos;
-	sanitizer_t *sanitizer;
+  state_machine_t *state_machine;
+  analyzer_t *analyzer;
+  hv_plate_t *
+      hv_plate; // TODO add hv plate interal data to analyzer to remove hv_plate
+  acc_data_t *acc_data;
+  bms_algos_t *bms_algos;
+  sanitizer_t *sanitizer;
 } state_machine_args_t;
 
 /**
  * @brief args for vAnalyzer
  */
 typedef struct {
-	analyzer_t *analyzer;
-	state_machine_t *state_machine;
-	acc_data_t *acc_data;
-	hv_plate_t *hv_plate;
+  analyzer_t *analyzer;
+  state_machine_t *state_machine;
+  acc_data_t *acc_data;
+  hv_plate_t *hv_plate;
 } analyzer_args_t;
 
 /**
  * @brief args for vGetSegmentData
  */
 typedef struct {
-	acc_data_t *acc_data;
-	state_machine_t *state_machine;
+  acc_data_t *acc_data;
+  state_machine_t *state_machine;
 } acc_data_args_t;
 
 /**
  * @brief args for vHvPlate
  */
 typedef struct {
-	hv_plate_t *hv_plate;
-	analyzer_t *analyzer;
-	bms_algos_t *bms_algos;
-	state_machine_t *state_machine;
+  hv_plate_t *hv_plate;
+  analyzer_t *analyzer;
+  bms_algos_t *bms_algos;
+  state_machine_t *state_machine;
 } hv_plate_args_t;
 
 /**
  * @brief args for vSanitizer
  */
 typedef struct {
-	sanitizer_t *sanitizer;
-	analyzer_t *analyzer;
+  sanitizer_t *sanitizer;
+  analyzer_t *analyzer;
 } sanitizer_args_t;
 
 /**
  * @brief args for vBmsAlgorithms
  */
 typedef struct {
-	sanitizer_t *sanitizer;
-	analyzer_t *analyzer;
-	bms_algos_t *bms_algos;
+  sanitizer_t *sanitizer;
+  analyzer_t *analyzer;
+  bms_algos_t *bms_algos;
 } bms_algos_args_t;
 
 /**
- * @brief args for peripheral thread 
+ * @brief args for peripheral thread
  */
 typedef struct {
-	peripherals_t *peripherals;
+  peripherals_t *peripherals;
 } peripherals_args_t;
 
 /* Task args end */
@@ -438,35 +453,35 @@ typedef struct {
  * @brief Fault codes
  */
 enum {
-	FAULTS_CLEAR = 0x0,
+  FAULTS_CLEAR = 0x0,
 
-	/* Shepherd BMS faults */
-	CELLS_NOT_BALANCING = 0x1,
-	CELL_VOLTAGE_TOO_HIGH = 0x2,
-	CELL_VOLTAGE_TOO_LOW = 0x4,
-	PACK_TOO_HOT = 0x8,
-	WEAK_PACK_FAULT = 0x10,
-	EXTERNAL_CAN_FAULT = 0x20,
-	DISCHARGE_LIMIT_ENFORCEMENT_FAULT = 0x40,
-	CHARGE_LIMIT_ENFORCEMENT_FAULT = 0x80,
-	DIE_TEMP_MAXIMUM_FAULT = 0x100,
-	HV_PLATE_COMMS_FAULT = 0x200,
-	SEGMENT_COMMS_FAULT = 0x400,
+  /* Shepherd BMS faults */
+  CELLS_NOT_BALANCING = 0x1,
+  CELL_VOLTAGE_TOO_HIGH = 0x2,
+  CELL_VOLTAGE_TOO_LOW = 0x4,
+  PACK_TOO_HOT = 0x8,
+  WEAK_PACK_FAULT = 0x10,
+  EXTERNAL_CAN_FAULT = 0x20,
+  DISCHARGE_LIMIT_ENFORCEMENT_FAULT = 0x40,
+  CHARGE_LIMIT_ENFORCEMENT_FAULT = 0x80,
+  DIE_TEMP_MAXIMUM_FAULT = 0x100,
+  HV_PLATE_COMMS_FAULT = 0x200,
+  SEGMENT_COMMS_FAULT = 0x400,
 
-	MAX_FAULTS = 0x80000000 /* Maximum allowable fault code */
+  MAX_FAULTS = 0x80000000 /* Maximum allowable fault code */
 };
 
 /**
  * @brief Represents fault evaluation operators
  */
 typedef enum {
-	GT, /* fault if {data} greater than {threshold}             */
-	LT, /* fault if {data} less than {threshold}                */
-	GE, /* fault if {data} greater than or equal to {threshold} */
-	LE, /* fault if {data} less than or equal to {threshold}    */
-	EQ, /* fault if {data} equal to {threshold}                 */
-	NEQ, /* fault if {data} not equal to {threshold}             */
-	NOP /* no operation, use for single threshold faults        */
+  GT,  /* fault if {data} greater than {threshold}             */
+  LT,  /* fault if {data} less than {threshold}                */
+  GE,  /* fault if {data} greater than or equal to {threshold} */
+  LE,  /* fault if {data} less than or equal to {threshold}    */
+  EQ,  /* fault if {data} equal to {threshold}                 */
+  NEQ, /* fault if {data} not equal to {threshold}             */
+  NOP  /* no operation, use for single threshold faults        */
 
 } fault_evalop_t;
 
@@ -474,22 +489,22 @@ typedef enum {
  * @brief Represents data to be packaged into a fault evaluation
  */
 typedef struct {
-	char id[100];
-	nertimer_t timer;
+  char id[100];
+  nertimer_t timer;
 
-	float data_1;
-	fault_evalop_t optype_1;
-	float lim_1;
+  float data_1;
+  fault_evalop_t optype_1;
+  float lim_1;
 
-	int timeout;
-	int code;
+  int timeout;
+  int code;
 
-	fault_evalop_t optype_2;
-	float data_2;
-	float lim_2;
+  fault_evalop_t optype_2;
+  float data_2;
+  float lim_2;
 
-	bool is_critical;
-	// bool is_faulted; /* note: unused field */
+  bool is_critical;
+  // bool is_faulted; /* note: unused field */
 } fault_eval_t;
 
 #endif
