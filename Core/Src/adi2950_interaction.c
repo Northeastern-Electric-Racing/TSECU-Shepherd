3b23f1809fc3f0f21048910a37caf62bc98d36a0
Caio DaSilva <dasilva.cai@northeastern.edu>
2026-03-16 21:54:37 -0400

    fix queue

--- Core/Src/adi2950_interaction.c
+++ Core/Src/adi2950_interaction.c
@@ -4,6 +4,7 @@
 #include "adi_bms_utility.h"
 #include "pal.h"
 #include "u_tx_debug.h"
+#include "can_messages_tx.h"
 
 #define TOTAL_IC_2950 1
 
@@ -34,40 +35,42 @@ void write_config(cell_asic_2950 *ic, ACCI count)
 	adBmsWriteData2950(TOTAL_IC_2950, ic, WRCFGA2950, Config2950, A_2950);
 }
 
-void write_clear_flags_2950(cell_asic_2950 *ic) {
-    for (int cic = 0; cic < TOTAL_IC_2950; cic++) {
-        ic[cic].clflag.vdruv = CL_FLAG_SET2950;
-        ic[cic].clflag.ocmm = CL_FLAG_SET2950;
-	    ic[cic].clflag.oc3l = CL_FLAG_SET2950;
-	    ic[cic].clflag.ocagd_clrm = CL_FLAG_SET2950;
-	    ic[cic].clflag.ocal = CL_FLAG_SET2950;
-	    ic[cic].clflag.oc1l = CL_FLAG_SET2950;
+void write_clear_flags_2950(cell_asic_2950 *ic)
+{
+	for (int cic = 0; cic < TOTAL_IC_2950; cic++) {
+		ic[cic].clflag.vdruv = CL_FLAG_SET2950;
+		ic[cic].clflag.ocmm = CL_FLAG_SET2950;
+		ic[cic].clflag.oc3l = CL_FLAG_SET2950;
+		ic[cic].clflag.ocagd_clrm = CL_FLAG_SET2950;
+		ic[cic].clflag.ocal = CL_FLAG_SET2950;
+		ic[cic].clflag.oc1l = CL_FLAG_SET2950;
 
-	    ic[cic].clflag.vdduv = CL_FLAG_SET2950;
-	    ic[cic].clflag.noclk = CL_FLAG_SET2950;
-	    ic[cic].clflag.refflt = CL_FLAG_SET2950;
-	    ic[cic].clflag.ocbgd = CL_FLAG_SET2950;
-	    ic[cic].clflag.ocbl = CL_FLAG_SET2950;
-	    ic[cic].clflag.oc2l = CL_FLAG_SET2950;
+		ic[cic].clflag.vdduv = CL_FLAG_SET2950;
+		ic[cic].clflag.noclk = CL_FLAG_SET2950;
+		ic[cic].clflag.refflt = CL_FLAG_SET2950;
+		ic[cic].clflag.ocbgd = CL_FLAG_SET2950;
+		ic[cic].clflag.ocbl = CL_FLAG_SET2950;
+		ic[cic].clflag.oc2l = CL_FLAG_SET2950;
 
-	    ic[cic].clflag.vregov = CL_FLAG_SET2950;
-	    ic[cic].clflag.vreguv = CL_FLAG_SET2950;
-	    ic[cic].clflag.vdigov = CL_FLAG_SET2950;
-	    ic[cic].clflag.vdiguv = CL_FLAG_SET2950;
-	    ic[cic].clflag.sed1 = CL_FLAG_SET2950;
-	    ic[cic].clflag.med1 = CL_FLAG_SET2950;
-	    ic[cic].clflag.sed2 = CL_FLAG_SET2950;
-	    ic[cic].clflag.med2 = CL_FLAG_SET2950;
+		ic[cic].clflag.vregov = CL_FLAG_SET2950;
+		ic[cic].clflag.vreguv = CL_FLAG_SET2950;
+		ic[cic].clflag.vdigov = CL_FLAG_SET2950;
+		ic[cic].clflag.vdiguv = CL_FLAG_SET2950;
+		ic[cic].clflag.sed1 = CL_FLAG_SET2950;
+		ic[cic].clflag.med1 = CL_FLAG_SET2950;
+		ic[cic].clflag.sed2 = CL_FLAG_SET2950;
+		ic[cic].clflag.med2 = CL_FLAG_SET2950;
 
-	    ic[cic].clflag.vdel = CL_FLAG_SET2950;
-	    ic[cic].clflag.vde = CL_FLAG_SET2950;
-	    ic[cic].clflag.spiflt = CL_FLAG_SET2950;
-	    ic[cic].clflag.reset = CL_FLAG_SET2950;
-	    ic[cic].clflag.thsd = CL_FLAG_SET2950;
-	    ic[cic].clflag.tmode = CL_FLAG_SET2950;
-	    ic[cic].clflag.oscflt = CL_FLAG_SET2950;
+		ic[cic].clflag.vdel = CL_FLAG_SET2950;
+		ic[cic].clflag.vde = CL_FLAG_SET2950;
+		ic[cic].clflag.spiflt = CL_FLAG_SET2950;
+		ic[cic].clflag.reset = CL_FLAG_SET2950;
+		ic[cic].clflag.thsd = CL_FLAG_SET2950;
+		ic[cic].clflag.tmode = CL_FLAG_SET2950;
+		ic[cic].clflag.oscflt = CL_FLAG_SET2950;
 	}
-    adBmsWriteData2950(TOTAL_IC_2950, ic, CLRFLAG2950, Clrflag2950, NONE2950);
+	adBmsWriteData2950(TOTAL_IC_2950, ic, CLRFLAG2950, Clrflag2950,
+			   NONE2950);
 }
 
 uint16_t read_conversion_count_registers(cell_asic_2950 *ic)
@@ -145,29 +148,29 @@ void poll_and_read_aux_registers(cell_asic_2950 *ic)
 void set_gpo(cell_asic_2950 *ic, GPO_2950 gpo)
 {
 	switch (gpo) {
-	case GPO1_2950:
-		ic->tx_cfga.gpo1c = PULLED_DOWN;
-		break;
-	case GPO2_2950:
-		// NOTE: temporary change for enabling HV readings on devkit
-		// GPO2 is PUSH_PULL
-		ic->tx_cfga.gpo2od = PUSH_PULL;
-		ic->tx_cfga.gpo2c = PULLED_UP_TRISTATED;
-		break;
-	case GPO3_2950:
-		ic->tx_cfga.gpo3c = PULLED_DOWN;
-		break;
-	case GPO4_2950:
-		ic->tx_cfga.gpo4c = PULLED_DOWN;
-		break;
-	case GPO5_2950:
-		ic->tx_cfga.gpo5c = PULLED_DOWN;
-		break;
-	case GPO6_2950:
-		ic->tx_cfga.gpo6c = PULLED_DOWN;
-		break;
-	default:
-		break;
+		case GPO1_2950:
+			ic->tx_cfga.gpo1c = PULLED_DOWN;
+			break;
+		case GPO2_2950:
+			// NOTE: temporary change for enabling HV readings on devkit
+			// GPO2 is PUSH_PULL
+			ic->tx_cfga.gpo2od = PUSH_PULL;
+			ic->tx_cfga.gpo2c = PULLED_UP_TRISTATED;
+			break;
+		case GPO3_2950:
+			ic->tx_cfga.gpo3c = PULLED_DOWN;
+			break;
+		case GPO4_2950:
+			ic->tx_cfga.gpo4c = PULLED_DOWN;
+			break;
+		case GPO5_2950:
+			ic->tx_cfga.gpo5c = PULLED_DOWN;
+			break;
+		case GPO6_2950:
+			ic->tx_cfga.gpo6c = PULLED_DOWN;
+			break;
+		default:
+			break;
 	}
 
 	adBmsWakeupIc2950(TOTAL_IC_2950);
@@ -181,29 +184,29 @@ void set_gpo(cell_asic_2950 *ic, GPO_2950 gpo)
 void reset_gpo(cell_asic_2950 *ic, GPO_2950 gpo)
 {
 	switch (gpo) {
-	case GPO1_2950:
-		ic->tx_cfga.gpo1c = PULLED_UP_TRISTATED;
-		break;
-	case GPO2_2950:
-		// NOTE: temporary change for enabling HV readings on devkit
-		// GPO2 is PUSH_PULL
-		ic->tx_cfga.gpo2od = PUSH_PULL;
-		ic->tx_cfga.gpo2c = PULLED_DOWN;
-		break;
-	case GPO3_2950:
-		ic->tx_cfga.gpo3c = PULLED_UP_TRISTATED;
-		break;
-	case GPO4_2950:
-		ic->tx_cfga.gpo4c = PULLED_UP_TRISTATED;
-		break;
-	case GPO5_2950:
-		ic->tx_cfga.gpo5c = PULLED_UP_TRISTATED;
-		break;
-	case GPO6_2950:
-		ic->tx_cfga.gpo6c = PULLED_UP_TRISTATED;
-		break;
-	default:
-		break;
+		case GPO1_2950:
+			ic->tx_cfga.gpo1c = PULLED_UP_TRISTATED;
+			break;
+		case GPO2_2950:
+			// NOTE: temporary change for enabling HV readings on devkit
+			// GPO2 is PUSH_PULL
+			ic->tx_cfga.gpo2od = PUSH_PULL;
+			ic->tx_cfga.gpo2c = PULLED_DOWN;
+			break;
+		case GPO3_2950:
+			ic->tx_cfga.gpo3c = PULLED_UP_TRISTATED;
+			break;
+		case GPO4_2950:
+			ic->tx_cfga.gpo4c = PULLED_UP_TRISTATED;
+			break;
+		case GPO5_2950:
+			ic->tx_cfga.gpo5c = PULLED_UP_TRISTATED;
+			break;
+		case GPO6_2950:
+			ic->tx_cfga.gpo6c = PULLED_UP_TRISTATED;
+			break;
+		default:
+			break;
 	}
 
 	adBmsWakeupIc2950(TOTAL_IC_2950);
@@ -295,7 +298,15 @@ void count_hv_plate_pec_errors(cell_asic_2950 *ic)
 			}
 		}
 
-		//send_hv_plate_pec_error_message(pec_error_count);
+		send_hv_plate_pec_errors(
+			ic->cccrc.sid2950_pec > 0U, ic->cccrc.comm_pec > 0U,
+			ic->cccrc.stat_pec > 0U, ic->cccrc.flag_pec > 0U,
+			ic->cccrc.aux_pec > 0U, ic->cccrc.avgivbat_pec > 0U,
+			ic->cccrc.avgvbat_pec > 0U, ic->cccrc.avgcr_pec > 0U,
+			ic->cccrc.oc_pec > 0U, ic->cccrc.ivbat_pec > 0U,
+			ic->cccrc.vbat_pec > 0U, ic->cccrc.cr_pec > 0U,
+			ic->cccrc.cfgr_pec > 0U, ic->cccrc.rvr_pec > 0U,
+			ic->cccrc.vr_pec > 0U);
 	}
 
 	ic->cccrc.cfgr_pec = 0U;
