567f46f98ba9897fba0b139d61b8cc703c6871f6
Aolgeirson <asher@olgeirson.com>
2026-03-22 17:16:04 -0400

    rebasing

--- Core/Src/can_handler.c
+++ Core/Src/can_handler.c
@@ -1,10 +1,10 @@
 #include "can_handler.h"
-#include "datastructs.h"
-#include "u_queues.h"
-#include "state_machine.h"
-#include "u_tx_general.h"
-#include "u_tx_debug.h"
 #include "control.h"
+#include "datastructs.h"
+#include "state_machine.h"
+#include "u_queues.h"
+#include "u_tx_debug.h"
+#include "u_tx_general.h"
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,89 +14,84 @@
 extern FDCAN_HandleTypeDef hfdcan2;
 can_t can1;
 
-uint8_t init_can1(FDCAN_HandleTypeDef *hcan)
-{
-	/* Init CAN interface */
-	HAL_StatusTypeDef status = can_init(&can1, hcan);
-	if (status != HAL_OK) {
-		PRINTLN_ERROR(
-			"Failed to execute can_init() when initializing can1 (Status: %d/%s).",
-			status, hal_status_toString(status));
-		return U_ERROR;
-	}
+uint8_t init_can1(FDCAN_HandleTypeDef *hcan) {
+  /* Init CAN interface */
+  HAL_StatusTypeDef status = can_init(&can1, hcan);
+  if (status != HAL_OK) {
+    PRINTLN_ERROR(
+        "Failed to execute can_init() when initializing can1 (Status: %d/%s).",
+        status, hal_status_toString(status));
+    return U_ERROR;
+  }
 
-	/* Add filters for standard IDs */
-	uint16_t standard1[] = {DTI_CURRENT_CANID, BATTBOX_TEMP_CANID};
-	status = can_add_filter_standard(&can1, standard1);
-	if (status != HAL_OK) {
-		PRINTLN_ERROR(
-			"Failed to add standard filter to can1 (Status: %d/%s, ID1: %d, ID2: %d).",
-			status, hal_status_toString(status),
-			standard1[0], standard1[1]);
-		return U_ERROR;
-	}
+  /* Add filters for standard IDs */
+  uint16_t standard1[] = {DTI_CURRENT_CANID, BATTBOX_TEMP_CANID};
+  status = can_add_filter_standard(&can1, standard1);
+  if (status != HAL_OK) {
+    PRINTLN_ERROR("Failed to add standard filter to can1 (Status: %d/%s, ID1: "
+                  "%d, ID2: %d).",
+                  status, hal_status_toString(status), standard1[0],
+                  standard1[1]);
+    return U_ERROR;
+  }
 
-	uint16_t standard2[] = {CALYPSO_CONTROL_CANID, 0x00};
-	status = can_add_filter_standard(&can1, standard2);
-	if (status != HAL_OK) {
-		PRINTLN_ERROR(
-			"Failed to add standard filter to can1 (Status: %d/%s, ID1: %d, ID2: %d).",
-			status, hal_status_toString(status),
-			standard2[0], standard2[1]);
-		return U_ERROR;
-	}
+  uint16_t standard2[] = {CALYPSO_CONTROL_CANID, 0x00};
+  status = can_add_filter_standard(&can1, standard2);
+  if (status != HAL_OK) {
+    PRINTLN_ERROR("Failed to add standard filter to can1 (Status: %d/%s, ID1: "
+                  "%d, ID2: %d).",
+                  status, hal_status_toString(status), standard2[0],
+                  standard2[1]);
+    return U_ERROR;
+  }
 
-	/* Add fitlers for extended IDs */
-	uint32_t extended1[] = {CHARGERBOX_CANID, 0x00};
-	status = can_add_filter_extended(&can1, extended1);
-	if (status != HAL_OK) {
-		PRINTLN_ERROR(
-			"Failed to add extended filter to can1 (Status: %d/%s, ID1: %ld, ID2: %ld).",
-			status, hal_status_toString(status),
-			extended1[0], extended1[1]);
-		return U_ERROR;
-	}
+  /* Add fitlers for extended IDs */
+  uint32_t extended1[] = {CHARGERBOX_CANID, 0x00};
+  status = can_add_filter_extended(&can1, extended1);
+  if (status != HAL_OK) {
+    PRINTLN_ERROR("Failed to add extended filter to can1 (Status: %d/%s, ID1: "
+                  "%ld, ID2: %ld).",
+                  status, hal_status_toString(status), extended1[0],
+                  extended1[1]);
+    return U_ERROR;
+  }
 
-	PRINTLN_INFO("Ran can1_init().");
+  PRINTLN_INFO("Ran can1_init().");
 
-	return U_SUCCESS;
+  return U_SUCCESS;
 }
 
-static uint8_t receive_can_msg(can_msg_t can_msg)
-{
-	return queue_send(&can_incoming, &can_msg, TX_NO_WAIT);
+static uint8_t receive_can_msg(can_msg_t can_msg) {
+  return queue_send(&can_incoming, &can_msg, TX_NO_WAIT);
 }
 
-void can_receive_callback(FDCAN_HandleTypeDef *hcan, uint32_t RxFifo0ITs)
-{
-	/* If a message has just been recieved... */
-	if (RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE) {
-		can_msg_t message;
-		FDCAN_RxHeaderTypeDef rx_header;
+void can_receive_callback(FDCAN_HandleTypeDef *hcan, uint32_t RxFifo0ITs) {
+  /* If a message has just been recieved... */
+  if (RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE) {
+    can_msg_t message;
+    FDCAN_RxHeaderTypeDef rx_header;
 
-		if (HAL_FDCAN_GetRxMessage(hcan, FDCAN_RX_FIFO0, &rx_header,
-					   message.data) == HAL_OK) {
-			message.id = rx_header.Identifier;
-			message.id_is_extended =
-				(rx_header.IdType == FDCAN_EXTENDED_ID);
-			message.len = (uint8_t)rx_header.DataLength;
+    if (HAL_FDCAN_GetRxMessage(hcan, FDCAN_RX_FIFO0, &rx_header,
+                               message.data) == HAL_OK) {
+      message.id = rx_header.Identifier;
+      message.id_is_extended = (rx_header.IdType == FDCAN_EXTENDED_ID);
+      message.len = (uint8_t)rx_header.DataLength;
 
-			/* Check size */
-			if (rx_header.DataLength > 8) {
-				printf("[main.c/HAL_FDCAN_RxFifo0Callback()] ERROR: Recieved message "
-				       "is larger than 8 bytes.\n");
-				return;
-			}
+      /* Check size */
+      if (rx_header.DataLength > 8) {
+        printf("[main.c/HAL_FDCAN_RxFifo0Callback()] ERROR: Recieved message "
+               "is larger than 8 bytes.\n");
+        return;
+      }
 
-			/* Send message to incoming CAN queue */
-			receive_can_msg(message);
-		}
-	}
+      /* Send message to incoming CAN queue */
+      receive_can_msg(message);
+    }
+  }
 }
 
-uint8_t queue_can_msg(can_msg_t can_msg)
-{
-	return queue_send(&can_outgoing, &can_msg, TX_NO_WAIT);
+uint8_t queue_can_msg(can_msg_t can_msg) {
+  return queue_send(&can_outgoing, &can_msg, TX_NO_WAIT);
 }
 
 /**
@@ -105,68 +100,62 @@ uint8_t queue_can_msg(can_msg_t can_msg)
  * @param msg
  * @return float
  */
-float parse_dti_current(can_msg_t msg)
-{
-	int16_t curr = msg.data[2] << 8 | msg.data[3];
-	return ((float)curr) / 10;
+float parse_dti_current(can_msg_t msg) {
+  int16_t curr = msg.data[2] << 8 | msg.data[3];
+  return ((float)curr) / 10;
 }
-float parse_charger_current(can_msg_t msg)
-{
-	int16_t curr = msg.data[2] << 8 | msg.data[3];
-	return ((float)curr) / 10;
+float parse_charger_current(can_msg_t msg) {
+  int16_t curr = msg.data[2] << 8 | msg.data[3];
+  return ((float)curr) / 10;
 }
 
 // CAN RECIEVE THREAD
-void vCanReceive(ULONG thred_input)
-{
-	can_msg_t message;
-	for (;;) {
-		/* Process incoming messages */
-		while (queue_receive(&can_incoming, &message,
-				     TX_WAIT_FOREVER) == U_SUCCESS) {
-			switch (message.id) {
-				case CHARGERBOX_CANID:
-					// TODO process charger can message
-					break;
-				case DTI_CURRENT_CANID:
-					// TODO process charger can message
-					break;
-				case CALYPSO_CONTROL_CANID:
-					control_message_fans(message);
-					break;
-				case BATTBOX_TEMP_CANID:
-					control_message_fans1(message);
-					break;
-				default:
-					break;
-			}
-		}
-	}
+void vCanReceive(ULONG thred_input) {
+  can_msg_t message;
+  for (;;) {
+    /* Process incoming messages */
+    while (queue_receive(&can_incoming, &message, TX_WAIT_FOREVER) ==
+           U_SUCCESS) {
+      switch (message.id) {
+      case CHARGERBOX_CANID:
+        // TODO process charger can message
+        break;
+      case DTI_CURRENT_CANID:
+        // TODO process charger can message
+        break;
+      case CALYPSO_CONTROL_CANID:
+        control_message_fans(message);
+        break;
+      case BATTBOX_TEMP_CANID:
+        control_message_fans_lv(message);
+        break;
+      default:
+        break;
+      }
+    }
+  }
 }
 
 // CAN DISPATCH THREAD
-void vCanDispatch(ULONG thread_input)
-{
-	// INITIALIZING CAN
-	PRINTLN_INFO("INITIALIZED CAN");
+void vCanDispatch(ULONG thread_input) {
+  // INITIALIZING CAN
+  PRINTLN_INFO("INITIALIZED CAN");
 
-	can_msg_t message;
-	uint8_t status;
+  can_msg_t message;
+  uint8_t status;
 
-	for (;;) {
-		/* Process incoming messages */
-		while (queue_receive(&can_outgoing, &message,
-				     TX_WAIT_FOREVER) == U_SUCCESS) {
-			status = can_send_msg(&can1, &message);
-			if (status != U_SUCCESS) {
-				PRINTLN_WARNING(
-					"Failed to send message (on can1) after removing from "
-					"outgoing queue (Message ID: %ld) - Status %d",
-					message.id, status);
-			} else {
-				PRINTLN_INFO("Sent CAN message with ID: %ld",
-					     message.id);
-			}
-		}
-	}
+  for (;;) {
+    /* Process incoming messages */
+    while (queue_receive(&can_outgoing, &message, TX_WAIT_FOREVER) ==
+           U_SUCCESS) {
+      status = can_send_msg(&can1, &message);
+      if (status != U_SUCCESS) {
+        PRINTLN_WARNING("Failed to send message (on can1) after removing from "
+                        "outgoing queue (Message ID: %ld) - Status %d",
+                        message.id, status);
+      } else {
+        PRINTLN_INFO("Sent CAN message with ID: %ld", message.id);
+      }
+    }
+  }
 }
--- Core/Src/control.c
+++ Core/Src/control.c
@@ -1,9 +1,10 @@
 
 #include "control.h"
-#include "main.h"
-#include "datastructs.h"
 #include "can_messages_tx.h"
+#include "datastructs.h"
+#include "main.h"
 #include "shep_mutexes.h"
+#include "u_tx_debug.h"
 
 #define INIT_TIMEOUT_MS 10
 
@@ -11,129 +12,113 @@
 
 uint8_t calypso_signals[NUM_DEVICES];
 uint8_t control_device_signals[NUM_DEVICES];
-pwm_device_t device_fan0; 
+pwm_device_t device_fan0;
 pwm_device_t device_fan1;
 
+static HAL_StatusTypeDef _init_pwm_device(pwm_device_t *device) {
+  HAL_StatusTypeDef status;
 
-static HAL_StatusTypeDef _init_pwm_device(pwm_device_t *device)
-{
-	HAL_StatusTypeDef status;
-
-	status = HAL_TIM_PWM_Init(device->tim_handle);
-	if (status != HAL_OK) {
-		return status;
-	}
-	status = HAL_TIM_PWM_Start(device->tim_handle,
-				   device->channel_identifier);
-	return status;
+  status = HAL_TIM_PWM_Init(device->tim_handle);
+  if (status != HAL_OK) {
+    return status;
+  }
+  status = HAL_TIM_PWM_Start(device->tim_handle, device->channel_identifier);
+  return status;
 }
 
-static void _write_pwm_device(pwm_device_t *device, uint16_t duty)
-{
-	__HAL_TIM_SET_COMPARE(device->tim_handle, device->channel_identifier,
-			      duty);
+static void _write_pwm_device(pwm_device_t *device, uint16_t duty) {
+  __HAL_TIM_SET_COMPARE(device->tim_handle, device->channel_identifier, duty);
 }
 
-bool control_init_peripherals(void)
-{
-	device_fan0 = (pwm_device_t){
-		.tim_handle = &htim3,
-		.channel_identifier = TIM_CHANNEL_2,
-	};
+bool control_init_peripherals(void) {
+  device_fan0 = (pwm_device_t){
+      .tim_handle = &htim3,
+      .channel_identifier = TIM_CHANNEL_2,
+  };
 
-	device_fan1 = (pwm_device_t){
-		.tim_handle = &htim3,
-		.channel_identifier = TIM_CHANNEL_1,
-	};
+  device_fan1 = (pwm_device_t){
+      .tim_handle = &htim3,
+      .channel_identifier = TIM_CHANNEL_1,
+  };
 
-	bool status = _init_pwm_device(&device_fan0);
-	if (!status) {
-		return false;
-	}
-	status = _init_pwm_device(&device_fan1);
-	if (!status) {
-		return false;
-	}
-	return true;
+  bool status = _init_pwm_device(&device_fan0);
+  if (!status) {
+    return false;
+  }
+  status = _init_pwm_device(&device_fan1);
+  if (!status) {
+    return false;
+  }
+  return true;
 }
 
+void control_fan(float pack_high_temp) {
+  uint16_t duty;
+  if (pack_high_temp <= 30.0f) {
+    duty = _PERCENT_16(CONTROL_MIN_DUTY_FAN);
+  } else if (pack_high_temp <= 50.0f) {
+    duty = _PERCENT_16(50);
+  } else if (pack_high_temp <= 60.0f) {
+    duty = _PERCENT_16(75);
+  } else {
+    duty = _PERCENT_16(100);
+  }
 
+  // fan0
+  uint16_t duty_from_calypso = _PERCENT_16(calypso_signals[DEVICE_FAN0]);
+  if (duty_from_calypso > duty) {
+    duty = duty_from_calypso;
+  }
+  control_device_signals[DEVICE_FAN0] = (uint8_t)(duty >> 8);
+  _write_pwm_device(&device_fan0, duty);
 
-void control_fan(float pack_high_temp)
-{
-	uint16_t duty;
-	if (pack_high_temp <= 30.0f) {
-		duty = _PERCENT_16(CONTROL_MIN_DUTY_FAN);
-	} else if (pack_high_temp <= 50.0f) {
-		duty = _PERCENT_16(50);
-	} else if (pack_high_temp <= 60.0f) {
-		duty = _PERCENT_16(75);
-	} else {
-		duty = _PERCENT_16(100);
-	}
-
-	//fan0
-	uint16_t duty_from_calypso = _PERCENT_16(calypso_signals[DEVICE_FAN0]);
-	if (duty_from_calypso > duty) {
-		duty = duty_from_calypso;
-	}
-	control_device_signals[DEVICE_FAN0] = (uint8_t)(duty >> 8);
-	_write_pwm_device(&device_fan0, duty);
-
-	
-
-	//fan1
-	uint16_t duty1 = _PERCENT_16(calypso_signals[DEVICE_FAN1]);
-	control_device_signals[DEVICE_FAN1] = (uint8_t)(duty1 >> 8);
-	_write_pwm_device(&device_fan1, duty1);
+  // fan1
+  uint16_t duty1 = _PERCENT_16(calypso_signals[DEVICE_FAN1]);
+  // uint16_t duty1 = _PERCENT_16(100);
+  control_device_signals[DEVICE_FAN1] = (uint8_t)(duty1 >> 8);
+  _write_pwm_device(&device_fan1, duty1);
 }
 
-void control_message_fans(can_msg_t msg)
-{
-	// First byte is the requested duty cycle (0-100)
-	uint8_t duty = *(msg.data);
-	if (duty > 100) {
-		duty = 100;
-	}
-	calypso_signals[DEVICE_FAN0] = duty;
+void control_message_fans(can_msg_t msg) {
+  // First byte is the requested duty cycle (0-100)
+  uint8_t duty = *(msg.data);
+  if (duty > 100) {
+    duty = 100;
+  }
+  calypso_signals[DEVICE_FAN0] = duty;
 }
 
-void control_message_fans1(can_msg_t msg)
-{
-	uint8_t duty1 = *(msg.data);
-	if (duty1 > 100) {
-		duty1 = 100;
-	}
-	calypso_signals[DEVICE_FAN1] = duty1;
+void control_message_fans_lv(can_msg_t msg) {
+  uint8_t temp_c = *(msg.data);
+  uint8_t duty_cycle = temp_c >= 35 ? 100 : 75;
+  PRINTLN_INFO("duty_cycle = %d\n", duty_cycle);
+  calypso_signals[DEVICE_FAN1] = duty_cycle;
 }
 
 #undef _PERCENT_16
 
 // CONTROL THREAD
-void vControl(ULONG thread_input)
-{
-	PRINTLN_INFO("Starting Control thread...");
+void vControl(ULONG thread_input) {
+  PRINTLN_INFO("Starting Control thread...");
 
-	analyzer_t *analyzer = (analyzer_t *)thread_input;
+  analyzer_t *analyzer = (analyzer_t *)thread_input;
 
-	PRINTLN_INFO("Starting Control thread...");
+  PRINTLN_INFO("Starting Control thread...");
 
-	// Initialize peripherals for control
-	bool failed = !control_init_peripherals();
-	if (failed) {
-		PRINTLN_ERROR(
-			"Failed to initialize one or more peripherals.\n");
-	}
+  // Initialize peripherals for control
+  bool failed = !control_init_peripherals();
+  if (failed) {
+    PRINTLN_ERROR("Failed to initialize one or more peripherals.\n");
+  }
 
-	for (;;) {
-		mutex_get(&analyzer_mutex);
-		float pack_high_temp = analyzer->max_temp.val;
-		control_fan(pack_high_temp);
-		mutex_put(&analyzer_mutex);
+  for (;;) {
+    mutex_get(&analyzer_mutex);
+    float pack_high_temp = analyzer->max_temp.val;
+    control_fan(pack_high_temp);
+    mutex_put(&analyzer_mutex);
 
-		send_fan_duty_cycle_percentage(control_device_signals[DEVICE_FAN0]);
-		send_fan_duty_cycle_percentage(control_device_signals[DEVICE_FAN1]);
-
-		tx_thread_sleep(MS_TO_TICKS(100));
-	}
+    send_fan_duty_cycle_percentage(control_device_signals[DEVICE_FAN0]);
+    send_fan_duty_cycle_percentage(control_device_signals[DEVICE_FAN1]);
+    tx_thread_sleep(MS_TO_TICKS(100));
+  }
 }
