1c3cd3872b408c33584c8abd8dbc40555898fb90
Caio DaSilva <dasilva.cai@northeastern.edu>
2026-04-27 10:00:24 -0400

    fabs

--- Core/Src/state_machine.c
+++ Core/Src/state_machine.c
@@ -514,7 +514,7 @@ void update_eval_table(state_machine_args_t *state_machine_args)
 		fault_eval_table[CHARGE_LIMIT_ENFORCEMENT_FAULT] =
 			(fault_eval_t){ .id = "Charge Current Limit",
 					.timer = ovr_chgcurr_timer,
-					.data_1 = hv_plate->pack_current,
+					.data_1 = fabs(hv_plate->pack_current),
 					.optype_1 = GT,
 					.lim_1 = bms_algos->cont_CCL,
 					.timeout = OVER_CHG_CURR_TIME,
