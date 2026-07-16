7fadc68c0d2cb3c07c07aa4b6a594adaa7755509
KSMehta11 <kaushikmehta119@gmail.com>
2026-07-16 17:17:17 -0400

    can codegen fix

--- Core/Inc/can_messages_rx.h
+++ Core/Inc/can_messages_rx.h
@@ -427,7 +427,6 @@ typedef struct {
  bool RTDS_FAULT;
  bool LV_LOW_VOLTAGE_FAULT;
  bool PRECHARGE_FLOATING_FAULT;
- bool LATCHING_ACTIVE_FAULT;
 } faults_t;
 
 void receive_faults(const can_msg_t *message, faults_t *faults);
