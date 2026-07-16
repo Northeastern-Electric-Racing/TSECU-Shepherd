7fec5fb1f6041b4561338850061ce8dab401aab3
KSMehta11 <kaushikmehta119@gmail.com>
2026-07-16 16:57:39 -0400

    Add balancing algorithm mutexes

--- Core/Src/charging.c
+++ Core/Src/charging.c
@@ -4,6 +4,7 @@
 #include "bms_config.h"
 #include "c_utils.h"
 #include "analyzer.h"
+#include "shep_mutexes.h"
 
 /// @brief A struct to hold the original float value and the index originally,
 /// as that holds meaning
@@ -107,17 +108,22 @@ void handle_balance_cells(analyzer_t *analyzer, acc_data_t *acc_data)
 	static const int MAX_BAL_CHIP = 7;
 
 	// the low cell, eventually they all must get there
-	float low = analyzer->min_ocv.val;
+	float low = 0.0f;
 	// the margin above the low cell to ignore, which is usually X% of the delta, gate at 0
 	//float min_thresh = //fmaxf(analyzer->delt_ocv * 0.4f, 0.0f);
 	float min_thresh = 0.02f;
 
 	val_idexed_t new_ocv_map[NUM_CHIPS][NUM_CELLS_PER_CHIP] = { 0 };
 
+	mutex_get(&analyzer_mutex);
+	low = analyzer->min_ocv.val;
 	// first, sort and cleanup everything
 	chipsSelectionSort(analyzer, new_ocv_map);
+	mutex_put(&analyzer_mutex);
 
 	PWM_DUTY duty_cycle = pwm_duty_cycle_get();
+
+	mutex_get(&balancing_mutex);
 
 	/* Balance all cells above the threshold, using the sorted ocv map values but
    * preserve the indexes*/
@@ -141,4 +147,6 @@ void handle_balance_cells(analyzer_t *analyzer, acc_data_t *acc_data)
 			}
 		}
 	}
+
+	mutex_put(&balancing_mutex);
 }
